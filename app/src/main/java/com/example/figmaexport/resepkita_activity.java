f359463983774c4a67121ea114640b26afa07187
DeniLuqmantoro <denniluqmantoro@gmail.com>
2023-05-07 14:52:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/figmaexport/resepkita_activity.java
@@ -0,0 +1,56 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		resepkita
+	 *	@date 		Sunday 07th of May 2023 04:29:10 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.figmaexport;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class resepkita_activity extends Activity {
+
+	
+	private View _bg__resepkita_ek2;
+	private TextView resepkita_ek3;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private ImageView download_removebg_preview_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.resepkita);
+
+		
+		_bg__resepkita_ek2 = (View) findViewById(R.id._bg__resepkita_ek2);
+		resepkita_ek3 = (TextView) findViewById(R.id.resepkita_ek3);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		download_removebg_preview_1 = (ImageView) findViewById(R.id.download_removebg_preview_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
